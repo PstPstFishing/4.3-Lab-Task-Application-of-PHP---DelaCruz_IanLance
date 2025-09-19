088b31ab2807d93b348380ba2988494a7756a2e5
IamNotDoinganything <ianlancedelacruz@gmail.com>
2025-09-19 10:00:11 +0800

    Add files via upload
    Changes

--- register.php
+++ register.php
@@ -11,15 +11,34 @@ $_SESSION['email'] = $_POST['email'];
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <style>
+    .center {
+    margin: auto;
+    margin-top: 100px;
+    width: 50%;
+    border: 3px solid #000000;
+    padding: 50px;
+    text-align: center;
+    
+    }
+
+</style>
   </head>
+  
 
 
   <body>
-        <div class="container-md border border-dark bg-light text-center mx-auto p-2">
-            <form action="profile.php" method="post">
-               Name: <input type="text" name="name" required><br>
-               Age: <input type="text" name="age"  required><br>
-               E-mail: <input type="text" name="email" required><br>
+        <div class="center">
+            <form class="form" action="profile.php" method="post">
+               Name: <input type="text" name="name" required>
+               <br>
+               <br>
+               Age: <input type="text" name="age"  required>
+               <br>
+               <br>
+               E-mail: <input type="text" name="email" required>
+               <br>
+               <br>
                <input type="submit">
              </form>
         </div>
